c59995491479d3a49342dff277547b626996fd43
christopher <tomiwa_chris@yahoo.com>
2021-05-22 15:08:15 +0100

    setting up

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<v-container>
+  <h1>hello</h1>
+</v-container>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+  
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
